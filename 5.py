07b55127f45778d3dff4dfe03680f0b7c33bdf67
Natroness <nikeshpaudyal05@gmail.com>
2025-10-09 20:30:17 -0500

    Average of nft

---
+++ 5.py
@@ -0,0 +1,27 @@
+
+def average_nft_value(nft_collection):
+    values = []
+    
+    if not nft_collection:
+        return 0
+    
+    for nft in nft_collection:
+        values.append(nft["value"])
+    
+    return sum(values) / len(values)
+
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2},
+    {"name": "Urban Jungle", "creator": "ArtByAlex", "value": 4.5}
+]
+print(average_nft_value(nft_collection))
+
+nft_collection_2 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9},
+    {"name": "Sunset Serenade", "creator": "SunsetArtist", "value": 9.4}
+]
+print(average_nft_value(nft_collection_2))
+
+nft_collection_3 = []
+print(average_nft_value(nft_collection_3))
