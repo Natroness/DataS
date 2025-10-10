68fbe269edd0272482b47f26eb741db2dd488dc8
Natroness <nikeshpaudyal05@gmail.com>
2025-10-09 20:26:36 -0500

    new DATAS

---
+++ 3.py
@@ -0,0 +1,28 @@
+
+
+def extract_nft_names(nft_collection):
+    nft_names = []
+    for nft in nft_collection:
+        nft_names += nft["name"]
+    return nft_names
+
+#Example Usage:
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2}
+]
+
+nft_collection_2 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9}
+]
+
+nft_collection_3 = []
+
+print(extract_nft_names(nft_collection))
+print(extract_nft_names(nft_collection_2))
+print(extract_nft_names(nft_collection_3))
+#Example Output:
+
+['Abstract Horizon', 'Pixel Dreams']
+['Golden Hour']
+[]
